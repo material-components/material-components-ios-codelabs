99f2ad9f55d54db5d0b45a4a065e4fa44ad667ae
Ian Gordon <iangordon@google.com>
2018-04-16 00:43:30 -0400

    Swift Import

--- MDC-103/Swift/Complete/Shrine/Shrine/HomeViewController.swift
+++ MDC-103/Swift/Complete/Shrine/Shrine/HomeViewController.swift
@@ -1,35 +1,27 @@
-//
-//  HomeViewController.swift
-//  Shrine
-//
-//  Created by Ian Gordon on 4/16/18.
-//  Copyright © 2018 Google. All rights reserved.
-//
+/*
+ Copyright 2018-present the Material Components for iOS authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UICollectionViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    self.collectionView?.backgroundColor = .orange
+  }
 
 }
