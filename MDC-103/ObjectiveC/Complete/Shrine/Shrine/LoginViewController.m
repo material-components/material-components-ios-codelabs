6f9bc8a62b764ede4d946c7950559aeaf41bc992
Cody Weaver <codyleeweaver@gmail.com>
2019-03-15 10:01:46 -0400

    Update theming

--- MDC-103/ObjectiveC/Complete/Shrine/Shrine/LoginViewController.m
+++ MDC-103/ObjectiveC/Complete/Shrine/Shrine/LoginViewController.m
@@ -148,10 +148,12 @@
                               toTextInputController:self.usernameTextFieldController];
   [MDCTextFieldColorThemer applySemanticColorScheme:colorScheme
                               toTextInputController:self.passwordTextFieldController];
-  [MDCTextButtonColorThemer applySemanticColorScheme:colorScheme
+  [MDCContainedButtonColorThemer applySemanticColorScheme:colorScheme
                                             toButton:self.cancelButton];
   [MDCContainedButtonColorThemer applySemanticColorScheme:colorScheme
                                                  toButton:self.nextButton];
+  [self.nextButton setElevation:MDCShadowElevationRaisedButtonResting forState:UIControlStateNormal];
+  [self.nextButton setElevation:MDCShadowElevationRaisedButtonPressed forState:UIControlStateHighlighted];
 
   // TODO: Theme our interface with our typography
   id<MDCTypographyScheming> typographyScheme = [ApplicationScheme sharedInstance].typographyScheme;
